c0f73737d08220fc5e81ad708d8c7cdc8ab5079d
Luana Martins <luamartinstravel@gmail.com>
2024-01-21 15:03:54 -0300

    creating component style

--- src/components/CardNews.js
+++ src/components/CardNews.js
@@ -16,12 +16,22 @@ const cardLeft = document.createElement("div");
 cardLeft.setAttribute("class", "card_left");//adicionando a class
 
 const autor = document.createElement("span");
+autor.textContent = "By " + (this.getAttribute("autor") || "Anonymous");
+
 const linkTitleH1 = document.createElement("h1");
 const linkTitle = document.createElement("a");
+linkTitle.textContent = this.getAttribute("title");
+linkTitle.href = this.getAttribute("link-url");
 linkTitleH1.appendChild(linkTitle);
+
+
 const newsContent = document.createElement("p");
+newsContent.textContent = this.getAttribute("content");
+
 const buttonApply = document.createElement("button");
 const linkApply = document.createElement("a");
+linkApply.textContent = this.getAttribute("button");
+linkApply.href = this.getAttribute("link-apply");
 buttonApply.appendChild(linkApply);
 
 cardLeft.appendChild(autor);
@@ -33,6 +43,8 @@ cardLeft.appendChild(buttonApply);
 const cardRight = document.createElement("div");
 cardRight.setAttribute("class", "card_right");//adicionando a class
 const newsImage = document.createElement("img");
+newsImage.src = this.getAttribute("photo");
+newsImage.alt = "Darth Vader image";
 cardRight.appendChild(newsImage);
 
 
@@ -44,8 +56,80 @@ return componentRoot;
 }
 
 styles(){
-
+    const style = document.createElement("style");
+    style.textContent = `
+    .card {
+        width: 720px;
+        border: 1px solid gray;
+        display: flex;
+        justify-content: space-between;
+        padding: 1rem;
+    
+        box-shadow: 10px 10px 29px 0px rgba(0,0,0,0.75);
+        -webkit-box-shadow: 10px 10px 29px 0px rgba(0,0,0,0.75);
+        -moz-box-shadow: 10px 10px 29px 0px rgba(0,0,0,0.75);
+    }
+    
+    .card_left, .card_right {
+        display: flex;
+        flex-direction: column;
+        justify-content: center;
+    }
+    
+    .card_left span{
+        font-size: 13px;
+        color: rgb(156, 9, 34);
+        font-weight: bold;
+    }
+    
+    .card_left h1{
+        margin-top: 10px;
+        font-size: 25px;
+    }
+    
+    .card_left a{
+        color: black;
+        text-decoration: none;
+    }
+    
+    
+    .card_left p{
+        color: rgb(70,70,70);
+    }
+    
+    .card_right img {
+        width: 17rem;
+    }
+    
+    button {
+        padding: 10px 20px;
+        margin-top: 2rem;
+        width: 15rem;
+        border: none;
+        font-size: 10px;
+        border-radius: 7px;
+        letter-spacing: 4px;
+        font-weight: 700;
+        text-transform: uppercase;
+        transition: 0.5s;
+        transition-property: box-shadow;
+        background: rgb(69, 69, 69);
+        box-shadow: 0 0 25px rgb(86, 88, 90);
+      }
+      
+      button:hover {
+        box-shadow: 0 0 5px rgb(19, 20, 20),
+                    0 0 25px rgb(112, 114, 116),
+                    0 0 50px rgb(66, 67, 69),
+                    0 0 100px rgb(6, 7, 7);
+      }
+    
+      .card_left button a {
+        color: white;
+      }
+    `
+    return style;
 }
 }
 
-customElements.define("card-news", CardNews); //declaração do component criado
+customElements.define("card-news",Cardnews); //declaração do component criado
