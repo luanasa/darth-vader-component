418d46eefbc1552e613985aa6503f8cdbea2130e
Luana Martins <luamartinstravel@gmail.com>
2024-01-21 14:13:16 -0300

    building the component 'build'

--- src/components/CardNews.js
+++ src/components/CardNews.js
@@ -11,11 +11,31 @@ build() {
 const componentRoot = document.createElement("div");
 componentRoot.setAttribute("class", "card"); //adicionando a class
 
+//Estrutura card_left
 const cardLeft = document.createElement("div");
 cardLeft.setAttribute("class", "card_left");//adicionando a class
 
+const autor = document.createElement("span");
+const linkTitleH1 = document.createElement("h1");
+const linkTitle = document.createElement("a");
+linkTitleH1.appendChild(linkTitle);
+const newsContent = document.createElement("p");
+const buttonApply = document.createElement("button");
+const linkApply = document.createElement("a");
+buttonApply.appendChild(linkApply);
+
+cardLeft.appendChild(autor);
+cardLeft.appendChild(linkTitleH1);
+cardLeft.appendChild(newsContent);
+cardLeft.appendChild(buttonApply);
+
+//Estrutura card_right
 const cardRight = document.createElement("div");
 cardRight.setAttribute("class", "card_right");//adicionando a class
+const newsImage = document.createElement("img");
+cardRight.appendChild(newsImage);
+
+
 
 componentRoot.appendChild(cardLeft); //pendurando ao componentRoot 'div principal'
 componentRoot.appendChild(cardRight);//pendurando ao componentRoot 'div principal'
