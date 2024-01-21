751c05f6dace5a205c245e82684397b9ea9c971c
Luana Martins <luamartinstravel@gmail.com>
2024-01-21 14:00:21 -0300

    setando propriedades de classes

--- src/components/CardNews.js
+++ src/components/CardNews.js
@@ -1,10 +1,31 @@
-class CardNews extends HTMLElement { // CardNews herda as carct. de um elemento HTML
+class Cardnews extends HTMLElement { // Cardnews herda as carct. de um elemento HTML
 constructor(){
     super();
 
     const shadow = this.attachShadow({ mode: "open"});
-    shadow.innerHTML = "<h1>Isso aqui é um componente</h1>";
+    shadow.appendChild(this.build()); //pendurando o build criado abaixo
+    shadow.appendChild(this.styles()); //pendurando o style criado abaixo
+}
+
+build() {
+const componentRoot = document.createElement("div");
+componentRoot.setAttribute("class", "card"); //adicionando a class
+
+const cardLeft = document.createElement("div");
+cardLeft.setAttribute("class", "card_left");//adicionando a class
+
+const cardRight = document.createElement("div");
+cardRight.setAttribute("class", "card_right");//adicionando a class
+
+componentRoot.appendChild(cardLeft); //pendurando ao componentRoot 'div principal'
+componentRoot.appendChild(cardRight);//pendurando ao componentRoot 'div principal'
+
+return componentRoot;
+}
+
+styles(){
+
 }
 }
 
-customElements.define("card-news", CardNews);
+customElements.define("card-news", CardNews); //declaração do component criado
